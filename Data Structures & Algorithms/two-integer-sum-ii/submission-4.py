68d1942a904a72900f4c376697898371f702eeaf
tim154753 <94345691+tim154753@users.noreply.github.com>
2026-09-01 19:28:33 -0700

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.py
@@ -0,0 +1,22 @@
+'''
+Note: list is sorted!
+- Binary search is O(log(n)), so a naive solution would be to iterate through nums and use binary search to see if target-num is in list. This would be O(nlogn).
+
+- Optimizations:
+    - After each num, if target-num is not in list, ignore it for next checks.
+        - Still doesn't get us down to O(n), but maybe on the right track?
+'''
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        front = 0
+        back = len(numbers) - 1
+        while front < back:
+            cur = numbers[front] + numbers[back]
+            if cur < target:
+                front += 1
+            elif cur > target:
+                back -= 1
+            else:
+                return [front+1, back+1]
