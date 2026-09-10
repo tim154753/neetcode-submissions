2484f0f2130d37a607800e5d7796dfaf5f0a4af7
tim154753 <94345691+tim154753@users.noreply.github.com>
2026-09-10 16:59:31 -0700

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,23 @@
+
+'''
+Brute force:
+    - At each i, iterate through list until we find index with higher temp. 
+
+How can a stack help?
+    - Under what conditions do we add an element to the stack?
+        - What about if it is greater than the element before it?
+'''
+
+from collections import deque
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = deque()
+        stack.append(0)
+        warmer = [0] * len(temperatures)
+        for i, temp in enumerate(temperatures):
+            while stack and (temp > temperatures[stack[-1]]):
+                idx = stack.pop()
+                warmer[idx] = i - idx
+            stack.append(i)
+        return warmer
+            
